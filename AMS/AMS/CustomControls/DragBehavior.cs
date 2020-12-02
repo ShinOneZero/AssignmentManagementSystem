74d813007c3163a1d5d21546c1014f75121c3551
ShinOneZero <60375879+ShinOneZero@users.noreply.github.com>
2020-12-03 07:02:35 +0900

    DragBehavior

---
+++ AMS/AMS/CustomControls/DragBehavior.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace AMS.CustomControls
+{
+    public class DragBehavior : Behavior<UIElement>
+    {
+        private Point elementStartPosition;
+        private Point mouseStartPosition;
+        private TranslateTransform transform = new TranslateTransform();
+
+        protected override void OnAttached()
+        {
+            Window parent = Application.Current.MainWindow;
+            AssociatedObject.RenderTransform = transform;
+
+            AssociatedObject.MouseLeftButtonDown += (sender, e) =>
+            {
+                mouseStartPosition = e.GetPosition(parent);
+                AssociatedObject.CaptureMouse();
+            };
+
+            AssociatedObject.MouseLeftButtonUp += (sender, e) =>
+            {
+                AssociatedObject.ReleaseMouseCapture();
+                elementStartPosition.X = transform.X;
+                elementStartPosition.Y = transform.Y;
+            };
+
+            AssociatedObject.MouseMove += (sender, e) =>
+            {
+                var mousePos = e.GetPosition(parent);
+                Vector diff = (mousePos - mouseStartPosition);
+                if (AssociatedObject.IsMouseCaptured)
+                {
+                    transform.X = elementStartPosition.X + diff.X;
+                    transform.Y = elementStartPosition.Y + diff.Y;
+                }
+            };
+        }
+    }
+}
